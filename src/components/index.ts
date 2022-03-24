69184c99940b2f0fb4f18c361a20059b105dbd27
Aakash Jha <aakash.jha1103@gmail.com>
2022-03-25 00:26:45 +0530

    Input Stepper component
    - increase/decrease step count by 1
    - light and dark mode

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button"
 export { default as Alert } from "./Alert"
 export { default as Label } from "./Label"
+export { default as InputStepper } from "./InputStepper"
