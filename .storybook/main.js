15e8aff67c25ad1397a88fe094ab7ba9cb623505
Aakash Jha <aakash.jha1103@gmail.com>
2022-06-08 23:23:26 +0530

    Card component and css module support
    - added support for `*.module.css` & `*.module.scss`
    - added new plugin to storybook to load module styles
    - added basic scaffold Card component (for testing)
    - added basic story for Card component (for testing)

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
 		"@storybook/addon-essentials",
 		"@storybook/addon-interactions",
 		"@storybook/addon-postcss",
+		"storybook-css-modules",
 	],
 	framework: "@storybook/react",
 }
